1d779857dbaf7592f886a7f055a6dcfe807ce0b9
HITANSH DOSHI <>
2020-11-03 17:06:12 +0530

    intial setting
    3 files already created index.html as main page.
    algo.js as a transition page.
    selection.js for selection sort not woring properly currently.

---
+++ algo.js
@@ -0,0 +1,54 @@
+var algo, array, arr_size;
+
+function set_height(arr, num){
+  // setting heights of all elements
+  let array_container = document.getElementById("array");
+  array_container.innerHTML = "";
+  for (var i = 0; i < num; i++) {
+    array_container.innerHTML += "<div class=\"arrayElement\" id=\"arr" + i + "\" style=\"height:" + arr[i] * 10 + "px;width:" + parseInt(screen.width / (2 * num)) + "px;\" >1</div>"
+  }
+}
+
+function set_array(num, choice) {
+  console.log(num, choice);
+  if (num == 0)
+    num = 5;
+  
+  // genrating Element with random height
+  let arr = [];
+  for (var i = 0; i < num; i++) {
+    arr[i] = Math.floor(Math.random() * 45) + 1;
+  }
+  
+  // for checking
+  console.log(screen.width / num, arr);
+  document.getElementById("data").innerHTML = arr;
+
+  set_height(arr, num);
+  
+  arr_size = num;
+  array = arr;
+  algo = choice;
+}
+
+function sort() {
+  menu = document.getElementById("menu");
+  console.log(menu);
+  menu.style.display="none";
+  
+  //sorting
+  switch (algo) {
+    case "Selection sort": 
+      set_paramenters(arr_size, array); 
+      window.requestAnimationFrame(Selection_sort);
+      break;
+    case "Bubble sort":
+      set_paramenters(arr_size, array); 
+      window.requestAnimationFrame(Selection_sort);
+      break;  
+    default:
+      break;
+  }
+}
+      
+    
---
+++ selection.js
@@ -0,0 +1,57 @@
+var i = 0, arr, num;
+
+function set_paramenters(n, a) {
+  num = n;
+  arr = a;
+  i = 0;
+}
+
+function set_bar_to(ele, color, time){
+  setTimeout(()=> {
+    window.requestAnimationFrame(()=>{ele.style.background = color;});
+  },time);
+}
+
+function swap_height(min_index){
+  a = document.getElementById("arr" + i);
+  b = document.getElementById("arr"+ min_index);
+  temp = a.style.height;
+  a.style.height = b.style.height;
+  b.style.height = temp;
+}
+
+function Selection_sort(){
+
+  var a = document.getElementById("arr" + i);
+  set_bar_to(a, "red", 0);
+
+  var min_index = i;
+  for (var j = i + 1; j < num; j++) {
+    b = document.getElementById("arr" + j);
+    set_bar_to(b, "green", 0);
+    if (arr[j] < arr[min_index])
+      min_index = j;
+
+    set_bar_to(b, "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)", 50);  
+  }
+  // changing elements of array
+  temp = arr[i];
+  arr[i] = arr[min_index];
+  arr[min_index] = temp;
+
+  set_bar_to = (a, "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)", 200);
+
+  swap_height(min_index);
+
+  if(++i<num){  
+    setTimeout(()=>
+      {
+        window.requestAnimationFrame(Selection_sort); 
+      },500);
+  }
+  else{
+    i=0;
+    menu = document.getElementById("menu");
+    menu.style.display="inline-block";
+  }
+}
